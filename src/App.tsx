f0b507c5c970a056fff55c16ed80f2492760760e
dmtrack <rightrack888@gmail.com>
2023-03-24 17:38:32 +0500

    feature: login form created

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
-import './App.css';
+import './styles/styles.css';
+import Chat from './scenes/chat/Chat';
+import Main from './scenes/main/Main';
+import { Routes, Route, Link } from 'react-router-dom';
+import NotFoundPage from './components/NotFoundPage';
 
-function App() {
-    return <div className='App'></div>;
-}
+const App: React.FC = () => {
+    return (
+        <div className='container'>
+            <Routes>
+                <Route path='/' element={<Main />} />
+                <Route path='/chat' element={<Chat />} />
+                <Route path='*' element={<NotFoundPage />} />
+            </Routes>
+        </div>
+    );
+};
 
 export default App;
---
+++ src/scenes/main/Main.tsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import main from '../../styles/main.module.css';
+
+interface IEnterRoom {
+    name: string;
+    room: string;
+}
+
+const Main = () => {
+    const [values, setValues] = useState<IEnterRoom>({
+        name: '',
+        room: '',
+    });
+
+    const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
+        setValues({ ...values, [e.currentTarget.name]: e.currentTarget.value });
+    };
+
+    const handleClick = (e: React.MouseEvent<HTMLElement>) => {
+        const isDisabled = Object.values(values).some(
+            (value: string) => !value
+        );
+        if (isDisabled) e.preventDefault();
+    };
+    return (
+        <>
+            (
+            <div className={main.wrap}>
+                <div className={main.container}>
+                    <h1 className={main.heading}>Join</h1>
+                    <form className={main.form}>
+                        {' '}
+                        <div className={main.group}>
+                            <input
+                                placeholder='name'
+                                type='text'
+                                name='name'
+                                value={values.name}
+                                className={main.input}
+                                autoComplete='off'
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className={main.group}>
+                            <input
+                                placeholder='room'
+                                type='text'
+                                name='room'
+                                value={values.room}
+                                className={main.input}
+                                onChange={handleChange}
+                                autoComplete='off'
+                                required
+                            />
+                        </div>
+                        <Link
+                            onClick={handleClick}
+                            className={main.group}
+                            to={`/chat/?name=${values.name}&room=${values.room}`}>
+                            <button type='submit' className={main.button}>
+                                Sign in
+                            </button>
+                        </Link>
+                    </form>
+                </div>
+                )
+            </div>
+        </>
+    );
+};
+
+export default Main;
